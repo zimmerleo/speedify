953b66a03d34cdfeca41e0a3b4861f6288014ce9
LZI <unconfigured@null.spigotmc.org>
2025-12-18 00:00:28 +0100

    initial commit

---
+++ src/main/java/de/seniorenheim/speedify/SpeedifyApplication.java
@@ -0,0 +1,13 @@
+package de.seniorenheim.speedify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeedifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/seniorenheim/speedify/SpeedifyApplicationTests.java
@@ -0,0 +1,13 @@
+package de.seniorenheim.speedify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
